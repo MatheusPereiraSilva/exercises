6f3e708c857c5c9f17eda6f89a07527e130d9ddd
Matheus Pereira <133778757+MatheusPereiraSilva@users.noreply.github.com>
2025-06-09 15:33:41 -0300

    Add files via upload

--- basicCalculator.py
+++ basicCalculator.py
@@ -1,33 +1,40 @@
-while True:
-    print('\nCalculadora Simples:')
-    print('1 - Adição')
-    print('2 - Subtração')
-    print('3 - Multiplicação')
-    print('4 - Divisão')
-    print('5 - Sair')
-
-    escolha = input('Escolha uma operação: ')
-
-    if escolha == '5':
-        print('Até mais!')
-        break
-    num1 = float(input('Digite o primeiro número: '))
-    num2 = float(input('Digite o segundo número: '))
-
-    if escolha == '1':
-        resultado = num1 + num2
-        print(f'O resultado é: {resultado}')
-    elif escolha == '2':
-        resultado = num1 - num2
-        print(f'O resultado é: {resultado}')
-    elif escolha == '3':
-        resultado = num1 * num2
-        print(f'O resultado é: {resultado}')
-    elif escolha == '4':
-        if num2 != 0:
-            resultado = num1 / num2
-            print(f'O resultado é: {resultado}')
-        else:
-            print('Erro de divisão por zero')
+def calculadora(operacao):
+    def soma(*args):
+        return sum(args)
+    def subtrair(*args):
+        resultado = args[0]
+        for numero in args[1:]:
+             resultado -= numero
+        return resultado
+    def multiplicacao(*args):
+        resultado = 1
+        for num in args:
+            resultado *= num
+        return resultado
+    def divisao(*args):
+        resultado = args[0]
+        for num in args[1: ]:
+            if num == 0:
+                raise ValueError("Divisão por zero não é permitida")
+            resultado /= num
+        return resultado
+    if operacao == 'soma':
+        return soma
+    elif operacao == 'subtrair':
+        return subtrair
+    elif operacao == 'multiplicação':
+        return  multiplicacao
+    elif operacao == 'divisão':
+        return divisao
     else:
-        print('Opção inválida. Tente novamente')
+        def operacaoNaoSuportada(*args):
+            return 'Operação não supoertada'
+        return operacaoNaoSuportada
+adicionar = calculadora('soma')
+print(adicionar(6, 4 ,2))
+subtrair = calculadora('subtrair')
+print(subtrair(8,4,2))
+multiplicar = calculadora('multiplicação')
+print(multiplicar(3, 2, 5))
+divisao = calculadora('divisão')
+print(divisao(80, 5))
